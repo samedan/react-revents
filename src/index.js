b11ba8fc0a5ab55e3484700fb29da2e8fa938a47
samedan <same.dan@gmail.com>
2019-01-17 17:30:37 +0100

    End of Section 12

--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ import './index.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 import ScrollToTop from './app/common/util/ScrollToTop';
+import { loadEvents } from './features/event/eventActions';
 
 const store = configureStore();
+store.dispatch(loadEvents());
 
 const rootEl = document.getElementById('root');
 
