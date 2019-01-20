222a28871ae785f79ecccc943e06ad37481916fa
samedan <same.dan@gmail.com>
2019-01-20 11:30:37 +0100

    end of section 18

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase';
 import 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCFQqqpQNDeWUcIpIAOI49EVVA4Srt-DOo',
-  authDomain: 'reactclientpanel-31428.firebaseapp.com',
-  databaseURL: 'https://reactclientpanel-31428.firebaseio.com',
-  projectId: 'reactclientpanel-31428',
-  storageBucket: 'reactclientpanel-31428.appspot.com',
-  messagingSenderId: '1053177882308'
-};
+const firebaseConfig = {};
 
 firebase.initializeApp(firebaseConfig);
 // const firestore = firebase.firestore();
